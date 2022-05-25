c970358b45f097262c2849ad8c5203f3e4de9e8c
Kirill <kirill.gorbatko@icloud.com>
2022-05-25 10:13:23 +0300

    fix()

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -16,7 +16,7 @@ window.onload = function () {
 	window.setTimeout(function () {
 		document.body.classList.add('loaded');
 		document.body.classList.remove('loaded_hiding');
-	}, 1500);
+	}, 1000);
 
 
 
